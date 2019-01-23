8d1b79f9eb945256aac56a7d3a834c915b8c78ad
yvonefan <yvonefan@163.com>
2019-01-23 10:03:42 +0800

    change release name which need to report detail

--- UtilDatabase.py
+++ UtilDatabase.py
@@ -21,7 +21,9 @@ class DatabaseHelper:
         :return:
         """
         try:
-            handler.Login('arsystem.isus.emc.com', 'dimsreport', 'report')
+            #TODO:
+            # handler.Login('arsystem.isus.emc.com', 'dimsreport', 'report')
+            handler.Login('arsappprd01.isus.emc.com', 'dimsreport', 'report')
             self.logger.debug("log in RemedyAR successfully ...") if self.logger else "No"
         except Exception, e:
             self.logger.debug(e) if self.logger else "No"
